39c9289742c676dad6d4c1f8da3cc4a543e4afb9
provash10 <provash20cb@gmail.com>
2024-10-16 18:34:25 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// External JS 
+
+var x = 50;
+var y = 20;
+var result1 = x + y;
+
+console.log(result1);
+
+var x = 50;
+var y = 20;
+var result2 = x + y + 7;
+
+console.log(result2);
+
+let a = 57;
+let b = 13;
+
+let result3 = a + b + 10;
+
+console.log(result3);
+
+// =============================================
+var rahi = 2000;
+var purchase = 1000;
+
+var remaining = rahi - purchase;
+
+console.log(remaining);
+
+// =====================================================
+
+// Rahim has 300 tk, went to market and purchase 5 eggs.
+// 2 kitkat chacholets. per eggs price 10 tk and per Kitkat 
+// prices 35 tk.
+// Find the Remainig Money?
+
+var total_tk = 300;
+
+var eggs = 5;
+var egg_price = 10;
+
+var egg_total = eggs * egg_price ;
+
+var kitkat = 2;
+var kitkat_price = 35;
+
+var kitkat_total = kitkat * kitkat_price;
+
+var total_cost = egg_total + kitkat_total;
+
+var remaining_tk = total_tk - total_cost;
+
+console.log(remaining_tk);
+
+
+
